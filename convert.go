c629aca6597ac5a270560938d081b63f1a59394c
Per Hultqvist <per@softteam.se>
2020-09-10 15:18:42 +0200

    using dwebp instead of convert

--- convert.go
+++ convert.go
@@ -2,43 +2,66 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/image/webp"
-	"image/jpeg"
+	log "github.com/hultan/softteam/log"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
 )
 
+var (
+	logger *log.Logger
+)
+
 func main() {
-	files, err := filepath.Glob("/softtube/thumbnails/*.webp")
+	// Setup logging
+	logger = log.NewLog(path.Join("/softtube/log/", "softtube.convert.log"))
+	defer logger.Close()
+
+	// Get files
+	filePaths, err := filepath.Glob("/softtube/thumbnails/*.webp")
 	if err != nil {
 		fmt.Printf("Error : %s", err.Error())
 		return
 	}
 
-	for _,file := range files {
-		convertWebp2Jpg(file)
+	// Log number of found files
+	msg := fmt.Sprintf("Found %d files that need converting.", len(filePaths))
+	logger.Log(msg)
+
+	// Convert found files
+	for _, filePath := range filePaths {
+		logger.Log(fmt.Sprintf("Converting file %s.", filePath))
+		err = convertWebp2Jpg(filePath)
+		if err != nil {
+			// We failed to convert using dwebp
+			logger.Log(fmt.Sprintf("Convert failed : %s",err.Error()))
+		}
 	}
+
+	// Log that we are finished
+	logger.Log("Finished converting.\n")
 }
 
-func convertWebp2Jpg(filePath string) error {
-	f, err := os.Open(filePath)
-	if err!=nil {
-		fmt.Printf("%s", err.Error())
-	}
-	img, err:=webp.Decode(f)
-	if err!=nil {
-		fmt.Printf("%s", err.Error())
+func convertWebp2Jpg(webpFilePath string) error {
+	jpgFilePath := getJpgFilename(webpFilePath)
+
+	// Run dwebp to convert the image
+	_, err := exec.Command("dwebp", webpFilePath,"-o", jpgFilePath).Output()
+	if err != nil {
+		return err
 	}
 
-	out,err := os.Create(getJpgFilename(filePath))
-	jpeg.Encode(out, img, nil)
-
-	err=os.Remove(filePath)
-	if err!=nil {
-		fmt.Printf("%s", err.Error())
-
+	// Remove webp file, if jpg exists
+	if _, err := os.Stat(jpgFilePath); os.IsNotExist(err) {
+		err = os.Remove(webpFilePath)
+		if err != nil {
+			return err
+		}
+		logger.Log(fmt.Sprintf("Removed file %s.", webpFilePath))
 	}
+
+	logger.Log(fmt.Sprintf("Successfully converted %s to %s.", webpFilePath, jpgFilePath))
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hultan/convert
 
 go 1.14
 
-require golang.org/x/image v0.0.0-20200801110659-972c09e46d76
+require (
+	github.com/hultan/softteam v0.1.0
+	golang.org/x/image v0.0.0-20200801110659-972c09e46d76
+)
